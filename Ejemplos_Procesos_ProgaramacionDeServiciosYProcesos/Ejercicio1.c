0a84a056376cabfd2e021aff1b462350ce6509ee
AlejandroSR33 <aserrev1102@g.educaand.es>
2022-09-29 12:56:43 +0200

    Project file

---
+++ Ejemplos_Procesos_ProgaramacionDeServiciosYProcesos/Ejercicio1.c
@@ -0,0 +1,8 @@
+//@author santi 2022/2023
+#include <stdio.h> //imprenscindible para las E/S
+#include <unistd.h> //imprescindible para las funciones con fork
+int main(){
+    fork();
+    printf("Soy un proceso, padre o Hijo?");
+    return 0;
+}
---
+++ Ejemplos_Procesos_ProgaramacionDeServiciosYProcesos/Ejercicio2.c
@@ -0,0 +1,8 @@
+//@author santi 2022/2023
+#include <stdio.h> //imprenscindible para las E/S
+#include <unistd.h> //imprescindible para las funciones con fork
+int main(){
+    printf("Soy el padre, por cojones y generaré 8 procesos en forma de arbol binario\n");
+    fork(); //po ó p1
+    
+}
