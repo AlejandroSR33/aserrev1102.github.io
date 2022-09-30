6ec7fadcb04803ccb9719f0a51ccfa577eae6c4c
AlejandroSR33 <aserrev1102@g.educaand.es>
2022-09-30 23:54:41 +0200

    up to date

--- Ejemplos_Procesos_ProgaramacionDeServiciosYProcesos/Ejercicio1.c
+++ Ejemplos_Procesos_ProgaramacionDeServiciosYProcesos/Ejercicio1.c
@@ -2,7 +2,8 @@
 #include <stdio.h> //imprenscindible para las E/S
 #include <unistd.h> //imprescindible para las funciones con fork
 int main(){
-    fork();//Este codigo se utiliza para crear el proceso
+    fork();//Este codigo se utiliza para crear un proceso
     printf("Soy un proceso, padre o Hijo?");
     return 0;
-}
+    
+}//Este programa crear un proceso padre y hace una copia del proceso padre como proceso hijo
--- Ejemplos_Procesos_ProgaramacionDeServiciosYProcesos/Ejercicio2.c
+++ Ejemplos_Procesos_ProgaramacionDeServiciosYProcesos/Ejercicio2.c
@@ -19,4 +19,12 @@ int main(){
     **/
     printf("Soy padre o un hijo?? \n");
     return 0;
+    /**
+    En este caso, este programa crea 8 procesos.
+    En el primero ciclo o fork se crea un proceso padre y se hace una copia del proceso padre com proceso hijo
+    En el segundo ciclo el padre crea un segundo hijo y el primer hijo pasa a ser el segundo padre y el segundo padre crea un tercer hijo
+    En el tercer ciclo el padre crea un cuarto hijo y el segundo padre crea un quinto hijo. Ademas de que el segundo hijo y tercer hijo, pasan a ser
+    tercer padre y cuarto padre, por lo tanto tendran un sexto hijo y un septimo hijo
+    **/
+
 }
